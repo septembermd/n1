f67c127df0ee443fab45560569b5dfe89d6f9480
Victor D <mdseptember@gmail.com>
2015-05-20 12:12:38 +0300

    Custom error page

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -7,11 +7,24 @@ $this->breadcrumbs = [
     'Error',
 ];
 ?>
-
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-    <p><?php echo CHtml::encode($message); ?></p>
-
-    <p><?php echo CHtml::link('Return to Home page', ['site/index']); ?></p>
+<div class="error-container">
+    <div class="error-code">
+        <?php echo $code; ?>
+    </div>
+    <div class="error-text">
+        <?php echo CHtml::encode($message); ?>
+    </div>
+    <div class="error-subtext">
+        <?php echo Yii::t('main', "Unfortunately we're having trouble loading the page you are looking for. Please wait a moment and try again or use action below."); ?>
+    </div>
+    <div class="error-actions">
+        <div class="row">
+            <div class="col-md-6">
+                <?php echo CHtml::link('Return to Home page', ['order/index'], ['class' => 'btn btn-info btn-block btn-lg']); ?>
+            </div>
+            <div class="col-md-6">
+                <button onclick="history.back();" class="btn btn-primary btn-block btn-lg">Previous page</button>
+            </div>
+        </div>
+    </div>
 </div>
