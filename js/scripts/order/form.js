1a1d87e738e4c7e0f778d04dffd1202bf9336600
Victor D <mdseptember@gmail.com>
2015-05-21 20:56:11 +0300

    Date validation

--- js/scripts/order/form.js
+++ js/scripts/order/form.js
@@ -1,5 +1,16 @@
 'use strict';
 (function($) {
+
+    var $form = $('#order-form'),
+        $saveAsDraftButton = $('.save-order-as-draft'),
+        $submitButton = $('[type=submit]', $form),
+        $statusInput = $('#Order_status_id', $form),
+        $validUntilInput = $('#Order_valid_date', $form),
+        $loadUntilInput = $('#Order_load_date', $form),
+        $deliverDueInput = $('#Order_deliver_date', $form),
+        $addOrderItemBtn = $('.add-order-item'),
+        $supplierInput = $('#Order_supplier_id', $form);
+
     $(document)
         .on('click', '.remove-order-item', function(e){
             e.preventDefault();
@@ -9,16 +20,55 @@
             });
         });
 
-    var $form = $('#order-form'),
-        $saveAsDraftButton = $('.save-order-as-draft'),
-        $submitButton = $('[type=submit]', $form),
-        $statusInput = $('#Order_status_id', $form),
-        $addOrderItemBtn = $('.add-order-item'),
-        $supplierInput = $('#Order_supplier_id', $form);
-
-    // Trigger populating of dependent dropdowns
     $(window).load(function(){
+        // Trigger populating of dependent dropdowns
         $supplierInput.trigger('change');
+        setTimeout(function(){
+            $validUntilInput.trigger('change');
+        }, 300);
+    });
+
+    function validateDateCollisions() {
+        var validUntilDate = $validUntilInput.data('datepicker').date,
+            loadUntilDate = $loadUntilInput.data('datepicker').date,
+            deliverDueDate = $deliverDueInput.data('datepicker').date;
+
+        if (loadUntilDate > validUntilDate) {
+            $loadUntilInput.val('');
+        }
+        if (deliverDueDate < loadUntilDate || deliverDueDate > validUntilDate) {
+            $deliverDueInput.val('');
+        }
+    }
+
+    function setLoadUntilLimit() {
+        var dateEnd = $validUntilInput.val();
+        if (dateEnd) {
+            $loadUntilInput.datepicker('setEndDate', new Date(dateEnd));
+        }
+    }
+
+    function setDeliverDueLimit() {
+        var dateStart = $loadUntilInput.val();
+        var dateEnd = $validUntilInput.val();
+        if (dateStart && dateEnd) {
+            $deliverDueInput.datepicker('setStartDate', new Date(dateStart));
+            $deliverDueInput.datepicker('setEndDate', new Date(dateEnd));
+        }
+    }
+
+
+    // Validate dates
+    $validUntilInput.on('change', function() {
+        validateDateCollisions();
+        setLoadUntilLimit();
+        setDeliverDueLimit();
+    });
+    $loadUntilInput.on('focus', function() {
+        //$validUntilInput.trigger('change');
+    }).on('change', function(){
+        validateDateCollisions();
+        setDeliverDueLimit();
     });
 
     $addOrderItemBtn.on('click', function(e){
