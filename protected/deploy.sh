eb9ee06e2ff6ce2f07b45cb1ba011132690dbafa
Victor D <mdseptember@gmail.com>
2015-05-23 11:42:32 +0300

    Added some color printing to deploy script

--- protected/deploy.sh
+++ protected/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo "Checking for the updates from repository"
+#Some colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "\n${GREEN}Checking for the updates from repository...${NC}\n\n"
 git checkout master
 git stash
 git pull origin master
 
-echo "Updating dependencies"
+printf "\n${GREEN}Updating composer dependencies...${NC}\n\n"
 ./composer.phar update
 
-echo "Done. Don't forget to update database"
+printf "\n${GREEN}Done. Don't forget to update database!${NC}\n\n"
