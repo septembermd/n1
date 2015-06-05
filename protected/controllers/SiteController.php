4e35295e1f2a394a30e40aa7b8b10e9c18098076
Victor D <mdseptember@gmail.com>
2015-06-05 17:08:59 +0300

    Fixed login redirect bug

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -125,7 +125,7 @@ class SiteController extends Controller
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login()) {
-                $this->redirect(Yii::app()->user->returnUrl);
+                $this->redirect(['order/index']);
             }
 		}
 		// display the login form
